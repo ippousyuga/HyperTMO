6397130f85a47f27e4deaeee2be1bf0ab81b9e2d
ippousyuga <1099368067@qq.com>
2023-02-08 01:43:40 +0800

    modifications

--- model/HGCN.py
+++ model/HGCN.py
@@ -17,15 +17,14 @@ class HGCN(nn.Module):
 
 
     def forward(self, x, G):
-        x = self.hgc1(x, G)    # 隐藏层1卷积操作
-        x = F.leaky_relu(x, 0.25)   # 非线性函数：LeakyReLU(x)=max(0,x)+negative_slope∗min(0,x)
-        # torch.nn.functional.leaky_relu(input, negative_slope=0.01, inplace=False) → Tensor
+        x = self.hgc1(x, G)   
+        x = F.leaky_relu(x, 0.25) 
         x = F.dropout(x, self.dropout, training=self.training)
-        x = self.hgc2(x, G)    # 隐藏层2卷积操作
-        x = F.leaky_relu(x, 0.25)   # 非线性函数：LeakyReLU(x)=max(0,x)+negative_slope∗min(0,x)
+        x = self.hgc2(x, G)
+        x = F.leaky_relu(x, 0.25)
         x = F.dropout(x, self.dropout, training=self.training)
-        x = self.hgc3(x, G)    # 隐藏层3卷积操作
-        x = F.leaky_relu(x, 0.25)   # 非线性函数：LeakyReLU(x)=max(0,x)+negative_slope∗min(0,x)
+        x = self.hgc3(x, G)
+        x = F.leaky_relu(x, 0.25)
         x = self.clf(x)
         x = self.fc(x)
 
@@ -37,16 +36,16 @@ class HGCN_conv(nn.Module):
     def __init__(self, in_ft, out_ft, bias=True):
         super(HGCN_conv, self).__init__()
 
-        self.weight = Parameter(torch.Tensor(in_ft, out_ft))    # 根据input特征和output特征随机构造权重
+        self.weight = Parameter(torch.Tensor(in_ft, out_ft))
         if bias:
-            self.bias = Parameter(torch.Tensor(out_ft))     # if 设置偏置为True则对bias赋值；
+            self.bias = Parameter(torch.Tensor(out_ft))
         else:
             self.register_parameter('bias', None)
         self.reset_parameters()
 
     def reset_parameters(self):
         stdv = 1. / math.sqrt(self.weight.size(1))
-        self.weight.data.uniform_(-stdv, stdv)  # +-区间stdv均匀分布
+        self.weight.data.uniform_(-stdv, stdv)
         if self.bias is not None:
             self.bias.data.uniform_(-stdv, stdv)
 
